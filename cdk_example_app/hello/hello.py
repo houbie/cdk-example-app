ea5c7727ad8d479f0f35b664ea6e88ad6bedfea6
ihoubr <ivo.houbrechts@nike.com>
2021-05-04 18:02:10 +0200

    api-gateway to s3 integration

--- cdk_example_app/hello/hello.py
+++ cdk_example_app/hello/hello.py
@@ -8,5 +8,5 @@ def handler(event, context):
         'headers': {
             'Content-Type': 'text/plain'
         },
-        'body': 'Hello, CDK! You have hit {}\n'.format(event['path'])
+        'body': 'Hello, again! You have hit {}\n'.format(event['path'])
     }
