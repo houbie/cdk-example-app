9b1faffc7665f10819f02044fddedd6b6f9af540
ihoubr <ivo.houbrechts@nike.com>
2021-10-12 11:44:17 +0200

    add region to event-log table meta

--- src/cdk_example_app/common/event_log.py
+++ src/cdk_example_app/common/event_log.py
@@ -34,7 +34,8 @@ class EventLog(Model):
     """Event processing log"""
 
     class Meta:
-        table_name = os.environ.get("EVENT_LOG_TABLE", f"event-log-{os.environ['AWS_REGION']}")
+        table_name = os.environ.get("EVENT_LOG_TABLE", f"event-log-{os.environ.get('AWS_REGION', 'eu-west-1')}")
+        region = os.environ.get("AWS_REGION", "eu-west-1")
 
     s3_key = UnicodeAttribute(hash_key=True)
     s3_bucket = UnicodeAttribute(attr_name="bucket")
