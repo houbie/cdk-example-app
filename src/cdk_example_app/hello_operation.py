883890a8ce8b31b22731e302272304d6d42e4112
ihoubr <ivo.houbrechts@nike.com>
2021-10-12 09:15:19 +0200

    return plain json from hello handler

--- src/cdk_example_app/hello_operation.py
+++ src/cdk_example_app/hello_operation.py
@@ -1,5 +1,7 @@
+import os
+
 from flask import jsonify
 
 
 def hello_world():
-    return jsonify(greeting="Hello world!!")
+    return {"greeting": "Hello world!!", "region": os.environ["AWS_REGION"]}
